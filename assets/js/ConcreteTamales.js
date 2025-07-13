9c1eaca81e9f16dfafdaba456067aab4b1627074
Michael <m.benattou@live.fr>
2025-07-13 13:16:03 +0200

    ajout eventlistenner

--- assets/js/ConcreteTamales.js
+++ assets/js/ConcreteTamales.js
@@ -1,28 +1,51 @@
-function openMenuBurger() {
-    let nav = document.querySelector("nav");
-    let buttonMenuBurger = document.querySelector(".menu-burger");
-    let xmarkMenuBurger = document.querySelector(".xmark-menu");
-        nav.style.display = "flex";
-        buttonMenuBurger.style.display = "none";
-        xmarkMenuBurger.style.display = "block";
-    } 
 
+  document.addEventListener("DOMContentLoaded", () => {
+  let nav = document.querySelector("nav");
+  let buttonMenuBurger = document.querySelector(".menu-burger");
+  let xmarkMenuBurger = document.querySelector(".xmark-menu");
 
-function closeMenuBurger() {
-    let nav = document.querySelector("nav");
-    let buttonMenuBurger = document.querySelector(".menu-burger");
-    let xmarkMenuBurger = document.querySelector(".xmark-menu");
+  function updateDisplay() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      nav.style.display = "none";
+      buttonMenuBurger.style.display = "block";
+      xmarkMenuBurger.style.display = "none";
+    } else {
+      nav.style.display = "flex";
+      buttonMenuBurger.style.display = "none";
+      xmarkMenuBurger.style.display = "none";
+    }
+  }
 
+  updateDisplay();
+
+  window.addEventListener("resize", updateDisplay);
+
+  buttonMenuBurger.addEventListener("click", () => {
+    nav.style.display = "flex";
+    buttonMenuBurger.style.display = "none";
+    xmarkMenuBurger.style.display = "block";
+  });
+
+  xmarkMenuBurger.addEventListener("click", () => {
     nav.style.display = "none";
     buttonMenuBurger.style.display = "block";
     xmarkMenuBurger.style.display = "none";
-}
+  });
+});
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  let input = document.querySelector("#searchInput");
+  let magnifyingGlasses = document.querySelector("#icone-loupe");
 
-setInterval(()=> {     
-     if (window.matchMedia("(max-width: 768px)").matches) {
-      console.log("mobile");
+  input.addEventListener("input", () => {
+    if (input.value.length !== 0) {
+      magnifyingGlasses.style.display = "none";
     } else {
-      console.log("desktop");
-    }}, 500);
+      magnifyingGlasses.style.display = "block";
+    }
+  });
+});
+
+    
+
